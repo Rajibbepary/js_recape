b4421c95f705ab0125678008f07d753ab62f6431
Rajibbepary <rajib635356@gmail.com>
2025-04-25 09:24:26 +0600

    let var const protice

---
+++ if_else/if.js
@@ -0,0 +1,23 @@
+
+
+// var age = 18;
+// var hight = 44;
+// if(age > 20 || hight > 40){
+//     console.log ('Your are voter')
+// }else{
+//     console.log('your are not voter')
+// }
+
+
+const person = {
+    name:'Rajib',
+    age:30
+}
+person.name = 'Raj'
+console.log(person)
+
+let sum = 0;
+for(let i = 0; 0 < 10; i++){
+    sum = sum + i;
+}
+//nodeconsole.log(sum);
