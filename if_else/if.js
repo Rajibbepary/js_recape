825adb5ee546eb6b851a17068d7e0cf077c0bee6
Rajibbepary <rajib635356@gmail.com>
2025-04-25 16:22:44 +0600

    map use

--- if_else/if.js
+++ if_else/if.js
@@ -9,15 +9,30 @@
 // }
 
 
-const person = {
-    name:'Rajib',
-    age:30
-}
-person.name = 'Raj'
-console.log(person)
 
-let sum = 0;
-for(let i = 0; 0 < 10; i++){
-    sum = sum + i;
-}
-//nodeconsole.log(sum);
+// function add (x, y){
+//     const sum = x + y;
+//     console.log(sum)
+//     return sum;
+
+// }
+// Refactored add function using arrow syntax and fixed argument passing
+
+const add = (x, y) => {
+    const sum = x + y;
+    console.log(sum);
+    return sum;
+};
+
+const sum = add(10, 5); // Added missing second argument
+
+const num = [ 12, 15, 14, 80]
+
+const Num =  num.map(n => n *2)
+console.log(Num)
+
+const friend = ['Rajib', 'Hason', 'Salam']
+const length = friend.map(frd => frd.length)
+console.table(length)
+const Fastl = friend.map(frd => frd[0]);
+console.table(Fastl)
